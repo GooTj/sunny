1ba9a38155074ed85e18efa76a11f18c03a27fc3
guzongren <guzongren@icloud.com>
2019-05-15 18:44:45 +0800

    eureka2

---
+++ src/main/java/com/onepiece/sunny/SunnyApplication.java
@@ -0,0 +1,15 @@
+package com.onepiece.sunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SunnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyApplication.class, args);
+    }
+
+}
